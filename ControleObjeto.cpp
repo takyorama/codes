c2d8a2360179f4b38f3b820b0cac477bde2a6f5f
takyorama <takyorama@hotmail.com>
2020-08-29 08:42:09 -0300

    Exemplo de camada controller
    Camada de controle inserida dentro do projeto

---
+++ ControleObjeto.cpp
@@ -0,0 +1,11 @@
+/**
+ * Sempre que for criar um projeto MVC,
+ * colocá-lo como namespace principal
+ * depois incluir os elementos como seus componentes
+ */
+
+namespace Projeto{
+   namespace **Model/View/Controller{
+      class ControllerObjeto(){};
+   }
+}
